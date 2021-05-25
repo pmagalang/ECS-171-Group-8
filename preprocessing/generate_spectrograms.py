f041aa45629bc0aa9d099370e023763cae97b54b
Paulo Magalang <53670831+pmagalang@users.noreply.github.com>
2021-05-24 20:48:09 -0700

    png files exported to correct directory

--- preprocessing/generate_spectrograms.py
+++ preprocessing/generate_spectrograms.py
@@ -12,7 +12,7 @@ def gen_melspectrogram(wav_file):
     librosa.display.specshow(mel_spect, fmax=8000)
     
     plt.axis('off')
-    plt.savefig('../Images/melspec.png', bbox_inches="tight", pad_inches = 0, transparent = True)
+    plt.savefig('../output/images/melspec.png', bbox_inches="tight", pad_inches = 0, transparent = True)
 
 def gen_Tonnetz(wav_file):
     y, sr = librosa.load(wav_file)
@@ -20,11 +20,11 @@ def gen_Tonnetz(wav_file):
     tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
     librosa.display.specshow(tonnetz)
     plt.axis('off')
-    plt.savefig('../Images/tonnetz.png', bbox_inches="tight",pad_inches = 0,transparent=True)
+    plt.savefig('../output/images/tonnetz.png', bbox_inches="tight",pad_inches = 0,transparent=True)
 
 def gen_Chroma(wav_file):
     y, sr = librosa.load(wav_file)
     y = librosa.effects.harmonic(y)
     librosa.display.specshow(librosa.feature.chroma_cqt(y, sr=sr), y_axis='chroma', x_axis='time')
     plt.axis('off')
-    plt.savefig('../Images/chroma.png', bbox_inches="tight",pad_inches = 0,transparent=True)
+    plt.savefig('../output/images/chroma.png', bbox_inches="tight",pad_inches = 0,transparent=True)
