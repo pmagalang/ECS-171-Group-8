cc5770a7942714c75be09df5d627653d921d54e8
t-thor <tjthorsen@ucdavis.edu>
2021-05-29 21:50:22 -0700

    added to3_split method
    takes single 30 second song and outputs ten 3 second clips

--- preprocessing/wav_splitter.py
+++ preprocessing/wav_splitter.py
@@ -15,5 +15,21 @@ def wav_split(filename):
             split_audio = audio[start:end]
             split_audio.export('output/audio/splitted{}.wav'.format(i), format="wav")
         
+def to3_split(filename):
+    audio = AudioSegment.from_wav(filename)
 
+    splits = []
+    milis = 0
+    for i in range(10):
+        splits.append(audio[milis:milis+2999])
+        milis+=3000
+
+    #get actual file name from path
+    filetitle = filename.split('/')[2]
+
+    #export all 10 three-second clips
+    for i in range(10):
+        splits[i].export('output/audio/{0}{1}.wav'.format(filetitle, i), format="wav")
+
+#def to3_direct(directory):
 
